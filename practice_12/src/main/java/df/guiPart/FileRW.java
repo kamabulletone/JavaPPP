a76a7af4c61db74d65bf62ab5a8313c9adb88cce
Lvov Tinley <tin-97@yandex.ru>
2021-02-13 15:14:02 +0300

    practice12

---
+++ practice_12/src/main/java/df/guiPart/FileRW.java
@@ -0,0 +1,87 @@
+package df.guiPart;
+
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import java.io.*;
+
+@Component
+public class FileRW implements RW{
+    private static String inputFile = "D:\\JavaPPPrepos\\practice_12\\input.txt";
+    private static String outputFile = "D:\\JavaPPPrepos\\practice_12\\output.txt";
+    private File inFile;
+
+    public FileRW() {
+        inFile = new File(inputFile);
+    }
+
+    @PostConstruct
+    public void createFile() {
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter(inputFile));
+//            FileWriter fw = new FileWriter(inFile);
+//            fw.write("Kostya");
+//            fw.write("Nikita");
+            Thread.sleep(500);
+            //writer.write("Kostya");
+            writer.flush();
+            System.out.println("i added smth");
+            read();
+        }
+        catch (IOException | InterruptedException e) {
+            System.out.println(e.getMessage());
+        }
+
+    }
+
+    @Override
+    public void read() {
+        try(BufferedReader reader = new BufferedReader(new FileReader(inputFile))) {
+
+            String value = reader.readLine();
+            reader.close();
+            if (value == null) {
+                write(null);
+            }
+            else {
+                String[] info = value.split(" ");
+                for (int i = 0; i < info.length; i++) {
+                    info[i] = Integer.toString(info[i].hashCode());
+                    write(info);
+                }
+            }
+
+        }
+        catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+
+    }
+
+    @Override
+    public void write(String[] data) {
+
+        try(BufferedWriter writer = new BufferedWriter(new FileWriter(outputFile))) {
+            if (data == null) {
+                writer.write("null");
+            }
+            else {
+                for (int i = 0; i < data.length; i++) {
+                    writer.write(data[i]);
+                }
+            }
+            delete();
+
+        }
+        catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+    @PreDestroy
+    private void delete() {
+        //inFile.delete();
+        System.out.println("file deleted");
+    }
+
+}
---
+++ practice_12/src/main/java/df/guiPart/RW.java
@@ -0,0 +1,9 @@
+package df.guiPart;
+
+
+public interface RW {
+
+    public void createFile();
+    public void read();
+    public void write(String[] info);
+}
