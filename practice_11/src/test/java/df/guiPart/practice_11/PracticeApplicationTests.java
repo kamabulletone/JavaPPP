1e41c9882344a1bde034d707b004bc409d4031cf
Lvov Tinley <tin-97@yandex.ru>
2021-02-13 13:26:00 +0300

    practice11 maven jar

---
+++ practice_11/src/test/java/df/guiPart/practice_11/PracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package df.guiPart.practice_11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
