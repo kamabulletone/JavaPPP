30998f5149f25f1e3305fc86d2870765c1d6bb49
Lvov Tinley <tin-97@yandex.ru>
2021-02-19 22:38:54 +0300

    fixed shutdown

--- practice_12/src/main/java/df/guiPart/Practice12Application.java
+++ practice_12/src/main/java/df/guiPart/Practice12Application.java
@@ -1,15 +1,36 @@
 package df.guiPart;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 
+import java.io.File;
+
 @SpringBootApplication
-@ComponentScan
+
+
 public class Practice12Application {
 
 	public static void main(String[] args) {
+
+//		ConfigurableApplicationContext ctx = new
+//				SpringApplicationBuilder(Conf.class).web(WebApplicationType.NONE).run();
+
+//		ApplicationContext context = new AnnotationConfigApplicationContext(Conf.class);
+//		FileRW fileRW = context.getBean(FileRW.class);
+
+
+		System.out.println("Spring Boot application started");
+
+		//SpringApplication.exit(ctx, () -> 0);
+
 		SpringApplication.run(Practice12Application.class, args);
+
 	}
 
 }
