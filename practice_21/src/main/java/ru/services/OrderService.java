53abe7481bd9ee8680e1229d20eb82f624ae6ab6
lvovtr <tin-97@yandex.ru>
2021-03-02 15:11:21 +0300

    added pr21

---
+++ practice_21/src/main/java/ru/services/OrderService.java
@@ -0,0 +1,56 @@
+package ru.services;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+import ru.model.Item;
+import ru.model.Order;
+import ru.repositories.OrderRepository;
+
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Service
+@Slf4j
+@Transactional
+public class OrderService {
+
+    private  OrderRepository reps;
+
+    public OrderService(OrderRepository reps) {
+        this.reps = reps;
+    }
+
+    public List<Order> getOrders() {
+        log.info("Find all orders");
+        return reps.findAll();
+    }
+
+    public void insertOrder(Order a) {
+        log.info("Save order {}", a);
+        reps.save(a);
+
+    }
+
+    public void deleteOrder(Order a) {
+        log.info("Delete order {}", a);
+        reps.delete(a);
+    }
+
+    public Item getItemsByOrder(String orderDate) {
+        log.info("Find item by orderDate \"{}\"", orderDate);
+        return reps.findById(orderDate).get().getItem();
+    }
+
+    public List<Order> orderByOrderDate() {
+        log.info("Find all orders order by orderDate");
+        return reps.findAll(Sort.by("orderDate"));
+    }
+
+    public List<Order> orderByItemName() {
+        log.info("Find all orders order by itemName");
+        return reps.findAll(Sort.by("itemName"));
+    }
+}
